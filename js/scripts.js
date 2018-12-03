23e7775a04b363414d7f2eea748f625ecaac8d98
Samantha <scdikolli@gmail.com>
2018-12-03 17:54:06 -0500

    improved music api

--- js/scripts.js
+++ js/scripts.js
@@ -85,7 +85,7 @@ function initMap() {
 //end Google Maps API
 
 //start Music API
- var url = 'https://api.musixmatch.com/ws/1.1/artist.albums.get?format=jsonp&callback=callback&artist_id=259675&apikey=4f16df4711bb025849083471398b8803';
+ var url = 'https://api.musixmatch.com/ws/1.1/album.tracks.get?format=jsonp&callback=callback&album_id=26806100&apikey=4f16df4711bb025849083471398b8803';
  var data = [];
  var albums = [];
  var html = '';
@@ -100,11 +100,15 @@ function initMap() {
    success:function(data){
      console.log(data);
 
-     albums=data.message.body.album_list;
+     albums=data.message.body.track_list;
 
      albums.forEach(function(album){
-       console.log(album);
-       html += '<div class="album-link">' + album.album_edit_url + '</div>';
+       console.log(album.track);
+
+       html += '<div class="track-cards flex">';
+         html += '<div class="track-title">' + album.track.track_name + '</div>';
+         html += '<a href="' + album.track.track_edit_url + '" target="_blank">' + '</div>';
+        html += '</div>';
      });
      $('#results').html(html);
    },//close success function
@@ -112,82 +116,4 @@ function initMap() {
      console.log("WTF");
    }
  });//close AJAX request
-
- function getLyrics(){
-
-var artistSearch = document.getElementById("artistSearch").value;
-document.getElementById("lyrics").textContent = "";
-  $.ajax({
-    type: "GET",
-    data: {
-        apikey:"445d6196c08dc2b7490929f18149d684",
-        q_artist: artistSearch,
-        format:"jsonp",
-        callback:"jsonp_callback"
-    },
-    url: "https://api.musixmatch.com/ws/1.1/track.search",
-    dataType: "jsonp",
-    jsonpCallback: 'jsonp_callback',
-    contentType: 'application/json',
-    success: function(data) {
-        console.log(data);
-        console.log(data.message.body.track_list[0].track.album_coverart_350x350)
-        console.log(data.message.body.track_list[0].track.lyrics_id)
-        var rand = data.message.body.track_list[Math.floor(Math.random() * data.message.body.track_list.length)];
-        console.log(rand.track.track_id)
-        var thisTrack = (rand.track.track_id)
-        var thisPic = rand.track.album_coverart_350x350;
-        console.log(thisPic)
-
-        var p = document.createElement("p");
-        p.textContent = thisTrack;
-        p.id = thisTrack;
-
-        var img = document.createElement("img")
-        img.setAttribute("src",thisPic)
-
-        document.getElementById("lyrics").appendChild(p).style.opacity = 0;
-        document.getElementById("lyrics").appendChild(img);
-        document.getElementById("ghost").click();
-
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR);
-        console.log(textStatus);
-        console.log(errorThrown);
-    }
-  });
- };
-
-
- function getLyricsNow(){
-    var trackId = document.getElementById("lyrics").textContent;
-    console.log(trackId)
-  $.ajax({
-    type: "GET",
-    data: {
-        apikey:"445d6196c08dc2b7490929f18149d684",
-        track_id: trackId,
-        format:"jsonp",
-        callback:"jsonp_callback"
-    },
-    url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get",
-    dataType: "jsonp",
-    jsonpCallback: 'jsonp_callback',
-    contentType: 'application/json',
-    success: function(data) {
-       console.log(data); console.log(data.message.body.lyrics.lyrics_body);
-      var lyricsBody = data.message.body.lyrics.lyrics_body.split(/\s+/).slice(0,100).join(" ")+ "...";
-
-        var j = document.createElement("p")
-        j.textContent = lyricsBody
-        document.getElementById("lyrics").appendChild(j)
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR);
-        console.log(textStatus);
-        console.log(errorThrown);
-    }
-  });
- };
  //end Music API
