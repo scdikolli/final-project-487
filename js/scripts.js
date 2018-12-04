0d76c4ec22c2c6aecfce167ffd8c053b252a3f91
Samantha <scdikolli@gmail.com>
2018-12-04 15:38:00 -0500

    added typewriter plugin, added content

--- js/scripts.js
+++ js/scripts.js
@@ -48,7 +48,7 @@ $(document).ready(function(){
 //start Google Maps API
 // Initialize and add the map
 function initMap() {
-  // The location of Glendale
+  // The locations
   var glendale = {lat: 33.5275, lng: -112.2625};
   var santaclara = {lat: 37.403, lng: -121.970};
   var pasadena = {lat: 34.161, lng: -118.168};
@@ -63,23 +63,48 @@ function initMap() {
   var eastrutherford = {lat: 40.813528, lng: -74.074361};
   var foxborough = {lat: 42.090944, lng: -71.264344};
   var pittsburgh = {lat: 40.446667, lng: -80.015833};
-  var atlanta = {lat: 33.5275, lng: -112.2625};
-  var tampa = {lat: 33.5275, lng: -112.2625};
-  var miamigardens = {lat: 33.5275, lng: -112.2625};
-  var nashville = {lat: 33.5275, lng: -112.2625};
-  var detroit = {lat: 33.5275, lng: -112.2625};
-  var minneapolis = {lat: 33.5275, lng: -112.2625};
-  var kansascity = {lat: 33.5275, lng: -112.2625};
-  var indianapolis = {lat: 33.5275, lng: -112.2625};
-  var stlouis = {lat: 33.5275, lng: -112.2625};
-  var neworleans = {lat: 33.5275, lng: -112.2625};
-  var houston = {lat: 33.5275, lng: -112.2625};
-  var arlington = {lat: 33.5275, lng: -112.2625};
+  var atlanta = {lat: 33.755, lng: -84.401};
+  var tampa = {lat: 27.975833, lng:  -82.503333};
+  var miamigardens = {lat: 25.958056, lng: -80.238889};
+  var nashville = {lat: 36.166389, lng: -86.771389};
+  var detroit = {lat: 42.34, lng:  -83.045556};
+  var minneapolis = {lat: 44.974, lng: -93.258};
+  var kansascity = {lat: 39.048889, lng: -94.483889};
+  var indianapolis = {lat: 39.760056, lng: -86.163806};
+  var stlouis = {lat: 38.632778, lng: -90.188611};
+  var neworleans = {lat: 33.755, lng: -84.401};
+  var houston = {lat: 29.684722, lng: -95.410833};
+  var arlington = {lat: 32.747778, lng: -97.092778};
   // The map, centered at Glendale
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 4, center: glendale});
-  // The marker, positioned at Glendale
-  var marker = new google.maps.Marker({position: glendale, map: map});
+  // The markers positioned at locations
+  var marker2 = new google.maps.Marker({position: glendale, map: map});
+  var marker3 = new google.maps.Marker({position: santaclara, map: map});
+  var marker4 = new google.maps.Marker({position: pasadena, map: map});
+  var marker5 = new google.maps.Marker({position: seattle, map: map});
+  var marker6 = new google.maps.Marker({position: denver, map: map});
+  var marker7 = new google.maps.Marker({position: chicago, map: map});
+  var marker8 = new google.maps.Marker({position: louisville, map: map});
+  var marker9 = new google.maps.Marker({position: columbus, map: map});
+  var marker10 = new google.maps.Marker({position: landover, map: map});
+  var marker11 = new google.maps.Marker({position: philadelphia, map: map});
+  var marker12 = new google.maps.Marker({position: cleveland, map: map});
+  var marker13 = new google.maps.Marker({position: eastrutherford, map: map});
+  var marker14 = new google.maps.Marker({position: foxborough, map: map});
+  var marker15 = new google.maps.Marker({position: pittsburgh, map: map});
+  var marker16 = new google.maps.Marker({position: atlanta, map: map});
+  var marker17 = new google.maps.Marker({position: tampa, map: map});
+  var marker18 = new google.maps.Marker({position: miamigardens, map: map});
+  var marker19= new google.maps.Marker({position: nashville, map: map});
+  var marker20 = new google.maps.Marker({position: detroit, map: map});
+  var marker21 = new google.maps.Marker({position: minneapolis, map: map});
+  var marker22 = new google.maps.Marker({position: kansascity, map: map});
+  var marker23 = new google.maps.Marker({position: indianapolis, map: map});
+  var marker24 = new google.maps.Marker({position: stlouis, map: map});
+  var marker25 = new google.maps.Marker({position: neworleans, map: map});
+  var marker26 = new google.maps.Marker({position: houston, map: map});
+  var marker27 = new google.maps.Marker({position: arlington, map: map});
 }
 
 //end Google Maps API
@@ -117,3 +142,10 @@ function initMap() {
    }
  });//close AJAX request
  //end Music API
+
+//Typewriter Plugin
+new TypeIt('#element', {
+  strings: ["7 months.", "38 shows.", "Over 2 million tickets sold.", "The highest-grossing tour in U.S. history."],
+  speed: 75,
+  autoStart:false
+})
